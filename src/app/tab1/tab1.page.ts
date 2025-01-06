af7444d9cb22aa0e85a41a0eb44b286ae49b499f
Michelle <myshall27@gmail.com>
2025-01-06 23:44:16 +0200

    adding basic function

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,45 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { IonHeader, IonToolbar, IonTitle, IonContent } from "@ionic/angular/standalone";
-import { ExploreContainerComponent } from "../explore-container/explore-container.component";
 
 @Component({
   selector: "app-tab1",
   templateUrl: "tab1.page.html",
   styleUrls: ["tab1.page.scss"],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, FormsModule],
 })
 export class Tab1Page {
-  constructor() {}
+  wah = 0;
+  balance: number = 0;
+
+  constructor() {
+    this.loadBalance();
+  }
+
+  addToBalance(amount: number) {
+    this.balance += amount;
+    this.clearInput();
+    localStorage.setItem("myBalance", `${this.balance}`);
+  }
+  deductFromBalance(amount: number) {
+    this.balance -= amount;
+    this.clearInput();
+    localStorage.setItem("myBalance", `${this.balance}`);
+  }
+  clearInput() {
+    this.wah = 0;
+  }
+  loadBalance() {
+    const balanceLocal = localStorage.getItem("myBalance");
+    if (balanceLocal === null) {
+      return;
+    }
+    try {
+      const parsedBalance = Number(balanceLocal);
+      this.balance = parsedBalance;
+    } catch (error) {
+      console.error(error);
+      localStorage.removeItem("myBalance");
+    }
+  }
 }
